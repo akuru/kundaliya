9fcb1630f311dd1ee5da5b986bcde4ce00c8aad5
Pubudu Kodikara <pubudu@leafycode.com>
2017-02-24 13:50:18 +0530

    Add initial app screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,77 @@
 import React, { Component } from 'react';
-
+import * as firebase from 'firebase';
 
 class App extends Component {
+  componentDidMount() {
+    const config = {
+      apiKey: 'AIzaSyCHJ_1he6bthXCTMU6r6pXPmcULyqtMFDU',
+      authDomain: 'kundaliya-test.firebaseapp.com',
+      databaseURL: 'https://kundaliya-test.firebaseio.com',
+      storageBucket: 'kundaliya-test.appspot.com',
+      messagingSenderId: '599295915329'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Kundaliya</h1>
+        <div className="main-content">
+          <h1 className="app-title">කුණ්ඩලිය / Kundaliya</h1>
+          <div className="kunadliya-description">
+            විරාම ලක්‍ෂණය කුණ්ඩලිය නමිවේ. මෙය යෙදෙන්නේ කිසියම් ශබ්දයක් සංකේතවත් කිරීමට නොවන
+            බැවින් සිංහල හෝඩියට අනුව අකුරක්වත් පිල්ලක් වත් නොවේ. වාක්‍යයක් හෝ ඡේදයක් හෝ අවසන්
+            වන බව හඟවනු වස් මෙම කුණ්ඩලිය යෙදෙයි. කුණ්ඩලිය, සිංහල භාෂාවට අනන්‍ය විරාම ලක්‍ෂණයකි
+          </div>
+          <h2 className="kunadliya">෴</h2>
+          <div className="app-description">
+            <span className="app-description-text">
+              Make your Own Kundaliya: Share your version of Sinhala Kundaliya
+            </span>
+          </div>
+          <br />
+          <div className="signin-container">
+            <div className="signin-text">
+              <span>Please sign in first :</span>
+            </div>
+
+            <button className="signin-btn google">
+              <i className="fa fa-google" aria-hidden="true" />
+              Sign in with Google
+            </button>
+
+            <button className="signin-btn facebook">
+              <i className="fa fa-facebook" aria-hidden="true" />
+              Sign in with Facebook
+            </button>
+
+            <button className="signin-btn github">
+              <i className="fa fa-github-alt" aria-hidden="true" />
+              Sign in with GitHub
+            </button>
+          </div>
+
+
+          {/* <button
+            onClick={() => {
+              const provider = new firebase.auth.GoogleAuthProvider();
+
+              firebase.auth().signInWithPopup(provider).then((result) => {
+                const token = result.credential.accessToken;
+                const user = result.user;
+                console.log('d', result);
+              }).catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                const email = error.email;
+                const credential = error.credential;
+                console.log('d', error);
+              });
+            }}
+          >G SignIn</button> */}
+        </div>
+
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import './styles/fonts.css';
 import './styles/App.css';
 
 ReactDOM.render(
