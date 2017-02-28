9a2300adc7ec66a58c1bbad81c283d83aaf62cb1
Pubudu Kodikara <pubudu@leafycode.com>
2017-02-28 11:45:09 +0530

    Fix notification styles

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import './styles/fonts.css';
 import './styles/App.css';
 import './styles/progress-button.css';
+import '../node_modules/notie/dist/notie.min.css';
 
 ReactDOM.render(
   <App />,
